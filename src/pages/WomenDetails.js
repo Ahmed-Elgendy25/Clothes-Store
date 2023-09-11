f453986c0c327ee8d246627ff1408a665b9fac2d
SugarAbuserr <202123713@o6u.edu.eg>
2023-09-11 17:51:38 +0300

    detailsCart + details sizes
    detailsCart & details sizes are handled.

--- src/pages/WomenDetails.js
+++ src/pages/WomenDetails.js
@@ -1,13 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ScrollTopButton from "../components/ScrollToTopButton";
 import { initialProducts } from "../components/WomenProducts";
+import { CartContext } from "../context/cart.js";
+import { notifySuccess } from "../components/notification";
+import CartComponent from "../components/CartComponent";
 
 export default function WomenDetails() {
   const { id } = useParams();
   const productDetails = initialProducts[id] || {};
+  const { addToCart } = useContext(CartContext);
+
+  const [selectedSize, setSelectedSize] = useState(null);
+
+  const handleSizeClick = (size) => {
+    setSelectedSize(size);
+  };
+
+  const handleAddToCartClick = () => {
+    if (selectedSize) {
+      addToCart(productDetails, selectedSize);
+      notifySuccess("Added to cart successfully");
+    }
+  };
 
   return (
     <>
@@ -26,21 +43,56 @@ export default function WomenDetails() {
               {productDetails.title}
             </h1>
             <div className="flex space-x-3 pt-5 items-center">
-              <span className="hover:bg-black hover:text-slate-100 rounded-full border-2 w-12 h-12 flex items-center justify-center">
-                XXS
-              </span>
-              <span className="hover:bg-black hover:text-slate-100 rounded-full border-2 w-12 h-12 flex items-center justify-center">
-                XS
-              </span>
-              <span className="hover:bg-black hover:text-slate-100 rounded-full border-2 w-12 h-12 flex items-center justify-center">
+              <button
+                onClick={() => handleSizeClick("S")}
+                className={`${
+                  selectedSize === "S"
+                    ? "bg-black text-slate-100"
+                    : "bg-slate-100 hover:bg-black hover:text-slate-100"
+                } rounded-full border-2 border-black w-12 h-12 flex items-center justify-center`}
+              >
                 S
-              </span>
-              <span className="hover:bg-black hover:text-slate-100 rounded-full border-2 w-12 h-12 flex items-center justify-center">
+              </button>
+              <button
+                onClick={() => handleSizeClick("M")}
+                className={`${
+                  selectedSize === "M"
+                    ? "bg-black text-slate-100"
+                    : "bg-slate-100 hover:bg-black hover:text-slate-100"
+                } rounded-full border-2 border-black w-12 h-12 flex items-center justify-center`}
+              >
                 M
-              </span>
-              <span className="hover:bg-black hover:text-slate-100 rounded-full border-2 w-12 h-12 flex items-center justify-center">
+              </button>
+              <button
+                onClick={() => handleSizeClick("L")}
+                className={`${
+                  selectedSize === "L"
+                    ? "bg-black text-slate-100"
+                    : "bg-slate-100 hover:bg-black hover:text-slate-100"
+                } rounded-full border-2 border-black w-12 h-12 flex items-center justify-center`}
+              >
+                L
+              </button>
+              <button
+                onClick={() => handleSizeClick("XL")}
+                className={`${
+                  selectedSize === "XL"
+                    ? "bg-black text-slate-100"
+                    : "bg-slate-100 hover:bg-black hover:text-slate-100"
+                } rounded-full border-2 border-black w-12 h-12 flex items-center justify-center`}
+              >
                 XL
-              </span>
+              </button>
+              <button
+                onClick={() => handleSizeClick("XXL")}
+                className={`${
+                  selectedSize === "XXL"
+                    ? "bg-black text-slate-100"
+                    : "bg-slate-100 hover:bg-black hover:text-slate-100"
+                } rounded-full border-2 border-black w-12 h-12 flex items-center justify-center`}
+              >
+                XXL
+              </button>
             </div>
             <p className="text-gray-600 pt-4 text-2xl">
               Price: {productDetails.price}
@@ -49,7 +101,10 @@ export default function WomenDetails() {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget
               quam nec nisl congue tincidunt. Integer euismod ex vel laoreet.
             </p>
-            <button className="w-full p-4 border-2 border-black text-white bg-black">
+            <button
+              className="w-full p-4 border-2 border-black text-white bg-black"
+              onClick={handleAddToCartClick}
+            >
               Add to Cart
             </button>
           </div>
@@ -57,6 +112,7 @@ export default function WomenDetails() {
       </div>
 
       <ScrollTopButton />
+      <CartComponent />
       <Footer />
     </>
   );
